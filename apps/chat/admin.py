e9f165ff07075108c313936c9dc294edff19bc7f
nyker510 <takamail53@gmail.com>
2021-05-01 11:08:41 +0900

    [add] admin site 表示

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+
+@admin.register(models.ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ('room_id', 'title', 'created_at',)
+
+
+@admin.register(models.Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('room', 'text', 'author',)
