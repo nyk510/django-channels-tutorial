71278bc747600b53e209fb2916e7644aec0c7420
nyker510 <takamail53@gmail.com>
2021-05-01 11:09:14 +0900

    [update] 過去のメッセージ表示 / tailwind でかわいくする

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,9 +1,18 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+
+from .models import ChatRoom
+
 
 def index(request):
     return render(request, 'chat/index.html')
 
+
 def room(request, room_name):
+    room = get_object_or_404(ChatRoom, room_id=room_name)
+
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'room': room,
+        'messages': room.messages.all()
     })
